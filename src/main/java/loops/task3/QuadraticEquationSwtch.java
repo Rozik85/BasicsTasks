381dccdad6b3e611cc260273455f78989ceae7c4
Dariusz <rozik85@o2.pl>
2018-01-03 02:29:55 +0100

    add task 2.3

---
+++ src/main/java/loops/task3/QuadraticEquationSwtch.java
@@ -0,0 +1,41 @@
+package loops.task3;
+
+import java.util.Scanner;
+
+public class QuadraticEquationSwtch {
+    public static void main(String[] args) {
+        double a, b, c, delta, x1, x2;
+        char liczbaPierwiastkow = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj a");
+        a = scanner.nextFloat();
+
+        if (a == 0) {
+            System.out.println("Niedozwolona wartość");
+        } else {
+            System.out.println("Podaj b");
+            b = scanner.nextFloat();
+            System.out.println("Podaj c");
+            c = scanner.nextFloat();
+            delta = Math.pow(b, 2) - (4 * a * c);
+            if (delta < 0) liczbaPierwiastkow = 0;
+            if (delta == 0) liczbaPierwiastkow = 1;
+            if (delta > 0) liczbaPierwiastkow = 2;
+
+            switch (liczbaPierwiastkow) {
+                case 0:
+                    System.out.println("Brak pierwiastkó");
+                    break;
+                case 1:
+                    x1 = -b / (2 * a);
+                    System.out.printf("Jeden pierwiastek:\nx1= %.2f\n", x1);
+                    break;
+                case 2:
+                    x1 = (-b - Math.sqrt(delta)) / (2 * a);
+                    x2 = (-b + Math.sqrt(delta)) / (2 * a);
+                    System.out.printf("Dwa pierwiastki:\nx1= %.2f \nx2= %.2f", x1, x2);
+                    break;
+            }
+        }
+    }
+}
