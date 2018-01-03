5f5c50284f6d0e3308df12305a939815960b3f78
Dariusz <rozik85@o2.pl>
2018-01-03 20:17:45 +0100

    add task 3.2

---
+++ src/main/java/loopDoWhileFor/task2/EquationDo.java
@@ -0,0 +1,15 @@
+package loopDoWhileFor.task2;
+
+public class EquationDo {
+    public static void main(String[] args) {
+        int y = 0, x = 0;
+        System.out.println("Program oblicza wartość funkcji y=3*x dla " +
+                "x zmieniającego się od 0 do 10");
+
+        do {
+            y = 3 * x;
+            System.out.println("x= " + x + "\t" + "y= " + y);
+            x++;
+        } while (x <= 10);
+    }
+}
