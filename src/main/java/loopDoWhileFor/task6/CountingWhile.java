1a32cb175793cf9710e4d47d39a8e6261576198b
Dariusz <rozik85@o2.pl>
2018-01-04 15:45:54 +0100

    add task 3.6

---
+++ src/main/java/loopDoWhileFor/task6/CountingWhile.java
@@ -0,0 +1,12 @@
+package loopDoWhileFor.task6;
+
+public class CountingWhile {
+    public static void main(String[] args) {
+        int i=1;
+        System.out.println("Program wyświetla liczby całkowite od 1 do 20.");
+        while (i <= 20) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
