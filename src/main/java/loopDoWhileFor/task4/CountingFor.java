839b19e9db6c2681e66532582d2306d0a4e906d2
Dariusz <rozik85@o2.pl>
2018-01-04 15:31:19 +0100

    add task 4

---
+++ src/main/java/loopDoWhileFor/task4/CountingFor.java
@@ -0,0 +1,9 @@
+package loopDoWhileFor.task4;
+
+public class CountingFor {
+    public static void main(String[] args) {
+        for (int i = 1; i < 21; i++) {
+            System.out.println(i);
+        }
+    }
+}
