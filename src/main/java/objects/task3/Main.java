7e999dd9461058a4e1a1eee6a886149bc2e1f401
Dariusz <rozik85@o2.pl>
2018-01-06 16:29:26 +0100

    add task 5.3

--- src/main/java/objects/task3/Main.java
+++ src/main/java/objects/task3/Main.java
@@ -1,10 +1,26 @@
 package objects.task3;
 
+/**
+ * 5.3 Napisz zgodnie z zasadami programowania obiektowego program,
+ który w tablicy 10×10 umieszcza losowo na przekątnej
+ liczby od 0 do 9, a poza przekątną zera. Dodatkowo program
+ oblicza sumę liczb znajdujących się na przekątnej. Klasa powinna
+ zawierać trzy metody z parametrami:
+ 􀁔 czytaj_dane(double [][]macierz, int rozmiar) — metoda
+ umieszcza dane w tablicy.
+ 􀁔 przetworz_dane(double [][]macierz, int rozmiar) — metoda
+ oblicza i wyświetla sumę liczb znajdujących się na
+ przekątnej.
+ 􀁔 wyswietl_wynik(double [][]macierz, int rozmiar) — metoda
+ wyświetla zawartość tablicy na ekranie monitora.
+ */
 public class Main {
     public static void main(String[] args) {
+        int size = 10;
+        double [][]macierz = new double[size][size];
         MotherTable motherTable=new MotherTable();
-        motherTable.read;
-        motherTable.proces;
-        motherTable.show;
+        motherTable.readData(macierz,size);
+        motherTable.processData(macierz,size);
+        motherTable.showData(macierz,size);
     }
 }
--- src/main/java/objects/task3/MotherTable.java
+++ src/main/java/objects/task3/MotherTable.java
@@ -1,5 +1,39 @@
 package objects.task3;
 
+import java.util.Random;
+
 public class MotherTable {
 
+    public void readData(double[][] macierz, int size) {
+        Random random = new Random();
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) {
+                    macierz[i][j] = random.nextInt(9);
+                } else {
+                    macierz[i][j] = 0;
+                }
+            }
+        }
+    }
+
+    public void processData(double[][] macierz, int size) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                sum += macierz[i][j];
+            }
+        }
+        System.out.println("Suma liczb po przekatnej : " + sum);
+    }
+
+    public void showData(double[][] macierz, int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print((int)macierz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 }
